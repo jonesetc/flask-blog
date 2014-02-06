496d61eef84f314829cb2f76eded475baf39a64d
Kyle Jones <kyle@jonesetc.com>
2014-02-05 18:27:54 -0600

    Modified models, added to .gitignore and added stubs for views

--- blog/models.py
+++ blog/models.py
@@ -3,38 +3,62 @@ from datetime import date
 
 from blog import db
 
+tags = db.Table('tags',
+    db.Column('tag_slug', db.String, db.ForeignKey('tag.slug')),
+    db.Column('post_slug', db.String, db.ForeignKey('post.slug'))
+)
+
 class Post(db.Model):
     """
     A post on the blog
 
-    :param int id: ID of this post (KEY)
+    :param str slug: Slug of this post (KEY)
     :param datetime.date date: Date of this post
     :param str title: Title of this post
     :param str body_md: Markdown version of this post
     :param str body_html: HTML version of this post (generated automatically from markdown)
     :param str css_file: Optional name of the custom css file
-    :param str user_email: Email of the user who posted this
+    :param str js_file: Optional name of the custom css file
+    :param str user_shortname: Shortname of the user who posted this
     :param User user: User who posted this
     """
     __tablename__ = 'post'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    slug = db.Column(db.String, primary_key=True)
     date = db.Column(db.Date, default=date.today())
     title = db.Column(db.String)
     body_md = db.Column(db.Text)
     body_html = db.Column(db.Text)
     css_file = db.Column(db.String, default=None, nullable=True)
-    user_email = db.Column(db.String, db.ForeignKey('user.email'))
+    js_file = db.Column(db.String, default=None, nullable=True)
+    user_shortname = db.Column(db.String, db.ForeignKey('user.shortname'))
     user = db.relationship('User', backref=db.backref('posts', lazy='dynamic'))
+    tags = db.relationship('Tag', secondary=tags,
+        backref=db.backref('posts', lazy='dynamic'))
 
     def __str__(self):
         return self.title
+
+class Tag(db.Model):
+    """
+    A tag for a post
+
+    :param str slug: Slug of this tag (KEY)
+    :param str name: Title of this tag
+    """
+    __tablename__ = 'tag'
+
+    slug = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String)
+
+    def __str__(self):
+        return self.name
 
 class User(db.Model):
     """
     A user of the blog
 
-    :param str email: Email of this user (KEY)
+    :param str shortname: Shortname of this user (KEY)
     :param str name: Name of this user
     :param str url: URL of this user
     :param str about_md: Markdown version of user's about section
@@ -44,7 +68,7 @@ class User(db.Model):
     """
     __tablename__ = 'user'
 
-    email = db.Column(db.String, primary_key=True)
+    shortname = db.Column(db.String, primary_key=True)
     name = db.Column(db.String)
     url = db.Column(db.String)
     about_md = db.Column(db.Text)
@@ -59,7 +83,7 @@ class User(db.Model):
         return True
 
     def get_id(self):
-        return self.email
+        return self.shortname
 
     def is_authenticated(self):
         return True
@@ -76,7 +100,7 @@ class Service(db.Model):
     :param str icon_file: Icon file of this service
     :param str alt_text: Optional alt-text for the icon
     :param str css_class: Optional css class of this icon
-    :param str user_emal: Email of this service's user
+    :param str user_shortname: Shortname of this service's user
     :param User user: This service's user
     """
     __tablename__ = 'service'
@@ -86,7 +110,7 @@ class Service(db.Model):
     icon_file = db.Column(db.String)
     alt_text = db.Column(db.String, default=None, nullable=True)
     css_class = db.Column(db.String, default=None, nullable=True)
-    user_email = db.Column(db.String, db.ForeignKey('user.email'))
+    user_shortname = db.Column(db.String, db.ForeignKey('user.shortname'))
     user = db.relationship('User', backref=db.backref('services', lazy='dynamic'))
 
     def __str__(self):
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,22 @@
 from blog import app
 
 @app.route('/')
-@app.route('/index')
-def index():
+def index_view():
     return "This will be an index"
+
+
+@app.route('/profile/<string:name>')
+def profile_view(name):
+    return "This will be a profile for the user named " + name
+
+@app.route('/user/<string:name>')
+def user_view(name):
+    return "This will be posts with the user named " + name
+
+@app.route('/post/<string:slug>')
+def post_view(slug):
+    return "This will be a post with the slug " + slug
+
+@app.route('/tag/<string:slug>')
+def tag_view(slug):
+    return "This will be posts with the tag slug " + slug
